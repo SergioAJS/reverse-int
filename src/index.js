b0ef75548d3406dabe6069034203398282a29c48
SergioAJS <SergFanMadrid@gmail.com>
2022-06-30 10:03:59 +0300

    feat: reverse solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  const str = n.toString();
+  let i = 0;
+  let result = '';
+  while (i < str.length) {
+    result = `${str[i]}${result}`;
+    i++
+  }
+  const reverseInt = parseInt(result);
+  return reverseInt;
 }
